ff8396cdc4219257c564bdd0a7ff2cf9d510b120
Shantha <sampathnsb@yahoo.com>
2020-11-19 23:19:33 +0000

    added dto and mapping to etitity <-> using AutoMapper

---
+++ src/AFIRegistration.Api/Profiles/CustomerProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace AFIRegistration.Api.Profiles
+{
+    public class CustomerProfile:Profile
+    {
+        public CustomerProfile()
+        {
+            CreateMap<Models.CustomerRegistrationDto, Entities.Customer>();
+            CreateMap<Entities.Customer, Models.CustomerRegistrationDto>();
+            CreateMap<Entities.Customer, Models.CustomerDto>();
+        }
+    }
+}
